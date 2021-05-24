382722def4adc3972f057c3d2fab4b0b05d656d7
Glitch (hello-webpage) <none>
2021-05-24 20:37:30 +0000

    🐔💐 Checkpoint ./mope/client.js:2102165/53

--- mope/client.js
+++ mope/client.js
@@ -20676,7 +20676,7 @@ _0x2af9ee = aniChoice_startT + 1000 * timeoutS;
     case 36:
       {  
         abil_dive_isMain =  msg.readUInt8();
-        abil_dive_recharging=  msg.readUInt8();
+        abil_dive_recharging=  msg.readUInt8() ;
         abil_recharging=  msg.readUInt8();
     if (abil_dive_isMain) {
      
