bb9c8a8b4a9cbbfcffa8190efe201c4f7dbc7f67
Glitch (hello-webpage) <none>
2021-06-15 22:20:10 +0000

    🍲🌼 Checkpoint ./mope/client.js:2102165/1118

--- mope/client.js
+++ mope/client.js
@@ -1635,9 +1635,7 @@ var drawStroke = function (x, y, rad, lineWidth, col) {
     ctx.arc(x, y, Math.max(0, rad), 0, Math.PI * 2);
     ctx.stroke();
 }
-  function ps(e) {
-        return Math.PI / 180 * e
-    }
+  
 
 
 
@@ -14186,7 +14184,7 @@ KingDragon.prototype.sF = 0.02;
 KingDragon.prototype.drawWings = function () {
     null == this.leftWingAnim && (this.leftWingAnim = new _0x1abe2b(this, this.flapDur, _0x1abe2b.wave,{
             'v1': 0x5
-        },false), this.leftWingAnim.keepLastFrame = true, this.leftWingAnim.loop = true, this.leftWingAnim.onFrameEntered = function (_0x1596df) {
+        },true), this.leftWingAnim.keepLastFrame = true, this.leftWingAnim.loop = true, this.leftWingAnim.onFrameEntered = function (_0x1596df) {
         this.forObj.frame1 = _0x1596df;
     });
     null != this.leftWingAnim && this.leftWingAnim.run();
@@ -14606,7 +14604,7 @@ Pterodactyl.prototype.getHead = function (_0x29679c, _0x4adc58) {
     return 'skins'+ '/' + _0x29679c + '/' + _0x29679c + '_head.png';
 };
 Pterodactyl.prototype.getSkinName = function () {
-          var _0x528bd8 = this.skinFolder() + '/pterodactyl/' + this.animalSpecies + '/pterodactyl';
+          var _0x528bd8 ='/pterodactyl/pterodactyl';
         return _0x528bd8 = 4 == this.specType ? _0x528bd8 + '4' : _0x528bd8 + (0 == this.specType ? '' : 1);
 };
 Pterodactyl.prototype.getWing = function (_0x4ac10e, _0x26897e) {
@@ -14669,6 +14667,7 @@ Pterodactyl.prototype.drawUnderSkinImg = function () {
     0x2 == this.specType && this.drawLegs();
 };
 Pterodactyl.prototype.drawSkinCustomization = function () {
+  this.beforeCustomDraw()
     if (this.flag_usingAbility) {
         this.drawBone(this.frame, 1);
         this.drawBone(this.frame, 2);
@@ -14686,10 +14685,11 @@ Pterodactyl.prototype.drawSkinCustomization = function () {
     } else if (_0x1dbcf1 = this.getHead('pterodactyl', true), _0x1dbcf1 = getLoadedImg(_0x1dbcf1)) _0x2e5ee9 = 1.4705882352941 * this.headScale, ctx.save(), _0x5b7869 = this.rad, ctx.drawImage(_0x1dbcf1, -_0x5b7869 * _0x2e5ee9, (-_0x5b7869 + _0x5b7869 * this.headX) * _0x2e5ee9, 2 * _0x5b7869 * _0x2e5ee9, 2 * _0x5b7869 * _0x2e5ee9), ctx.restore();
 };
 Pterodactyl.prototype.drawWing = function (_0x4f5a63, _0x5c300b) {
+  
     var _0x2a359f = 1 == _0x5c300b ? 1 : -1,
         _0x2bd995 = getLoadedImg(this.getWing('pterodactyl', _0x5c300b));
     if (_0x2bd995) {
-        var _0x22b0c1 = -(-0.2 + _0x4f5a63) * ps(_0x2a359f * this.r),
+        var _0x22b0c1 = -(-0.2 + _0x4f5a63) * toRadians(_0x2a359f * this.r),
             _0x15f848 = 0.8 * this.rad,
             _0x1f5264 = this.rad,
             _0x67c3e0 = _0x15f848 * this.ww,
@@ -14698,7 +14698,7 @@ Pterodactyl.prototype.drawWing = function (_0x4f5a63, _0x5c300b) {
             _0x24e8b9 = this.ay + this.yf * _0x4f5a63;
         ctx.save();
         _0x22b0c1 *= this.rf;
-        ctx.rotate(ps(_0x2a359f * (this.isGliding ? this.erf_gliding : this.erf)) + _0x22b0c1);
+        ctx.rotate(toRadians(_0x2a359f * (this.isGliding ? this.erf_gliding : this.erf)) + _0x22b0c1);
         ctx.drawImage(_0x2bd995, 0 + _0x67c3e0 * -_0x13ccc2 + _0x4f5a63 * _0x67c3e0 * _0x2a359f * this.wOffset, _0x1f5264 + _0x15f848 * -_0x24e8b9, _0x67c3e0, _0x15f848);
         ctx.restore();
     }
@@ -14707,7 +14707,7 @@ Pterodactyl.prototype.drawBone = function (_0xf1fb52, _0x48a7e2) {
     var _0x50e10e = getLoadedImg(this.getWingBone('pterodactyl', _0x48a7e2)),
         _0x152838 = 1 == _0x48a7e2 ? 1 : -1;
     if (_0x50e10e) {
-        var _0x24b193 = -(-0.2 + _0xf1fb52) * ps(_0x152838 * this.r),
+        var _0x24b193 = -(-0.2 + _0xf1fb52) * toRadians(_0x152838 * this.r),
             _0x22a484 = 0.8 * this.rad,
             _0x3f0bc9 = this.rad,
             _0x5a7c64 = _0x22a484 * this.ww,
@@ -14716,7 +14716,7 @@ Pterodactyl.prototype.drawBone = function (_0xf1fb52, _0x48a7e2) {
             _0x32ba27 = this.ay + this.yf * _0xf1fb52;
         ctx.save();
         _0x24b193 *= this.rf;
-        ctx.rotate(ps(_0x152838 * (this.isGliding ? this.erf_gliding : this.erf)) + _0x24b193);
+        ctx.rotate(toRadians(_0x152838 * (this.isGliding ? this.erf_gliding : this.erf)) + _0x24b193);
         ctx.drawImage(_0x50e10e, 0 + _0x5a7c64 * -_0x815a56 + _0xf1fb52 * _0x5a7c64 * _0x152838 * this.wOffset, _0x3f0bc9 + _0x22a484 * -_0x32ba27, _0x5a7c64, _0x22a484);
         ctx.restore();
     }
@@ -14726,7 +14726,7 @@ Pterodactyl.prototype.drawHand = function (_0x8ffe97, _0x50fe2f) {
     var _0x551de1 = 1 == _0x50fe2f ? 1 : -1,
         _0x48325e = getLoadedImg(this.getHand('pterodactyl', _0x50fe2f));
     if (_0x48325e) {
-        var _0x503254 = -(-0.2 + _0x8ffe97) * ps(_0x551de1 * this.r),
+        var _0x503254 = -(-0.2 + _0x8ffe97) * toRadians(_0x551de1 * this.r),
             _0x5026b3 = 0.8 * this.rad,
             _0x1b2913 = this.rad,
             _0x149abd = _0x5026b3 * this.ww,
