bc4a788be47be6dcf35c82fa7ac2326f12180b95
Glitch (hello-webpage) <none>
2021-06-25 17:58:44 +0000

    🍢🌾 Checkpoint ./mope/client.js:2102165/3

--- mope/client.js
+++ mope/client.js
@@ -4963,7 +4963,7 @@ if (KTestingModeON) {
     reg = "LOCAL";
 addServerDef("LOCALHOST", "127.0.0.1", reg,"80"); 
 //addServerDef("FFA", "35.246.216.142", reg,"80"); 
-addServerDef("FFA", "mopetestserverold.run-eu-central1.goorm.io", reg,"80");
+//saddServerDef("FFA", "mopetestserverold.run-eu-central1.goorm.io", reg,"80");
 //reg = "FFA";
  //  addServerDef("FFA", "146.148.81.224", reg);
   //   reg = "Team Mode";
