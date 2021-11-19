d9b21b118b21f76500e5170cf6f7279fca96c7ec
Glitch (hello-webpage) <none>
2021-11-19 16:03:43 +0000

    👱🍇 Checkpoint ./mope/client.js:36106651/126

--- mope/client.js
+++ mope/client.js
@@ -4979,8 +4979,8 @@ if (KTestingModeON) {
     reg = "Local Test";
   
   if(ACTIVATEOURGAMEMODE){addServerDef("FFA", "ahmetcan-channel-1.paiza-user-free.cloud/", reg,"80")}else{
-addServerDef("Local Test 1", "da47-190-215-158-88.eu.ngrok.io", reg,"80"); 
-addServerDef("Local Test 2", "da47-190-215-158-88.eu.ngrok.io", reg,"80"); 
+addServerDef("Local Test 1", "e7a3-190-215-158-88.ap.ngrok.io", reg,"80"); 
+//addServerDef("Local Test 2", "e7a3-190-215-158-88.ap.ngrok.io", reg,"80"); 
 
 //reg = "FFA";
  //  addServerDef("FFA", "146.148.81.224", reg);
