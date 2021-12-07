71efd13bb36720995dc825f8161f3982bda9a3a4
Glitch (hello-webpage) <none>
2021-12-07 15:22:15 +0000

    🏗🛰 Checkpoint ./.glitch-assets:39269466/380 ./mope/client.js:39269466/42

--- mope/client.js
+++ mope/client.js
@@ -5099,7 +5099,7 @@ if (KTestingModeON) {
     addServerDef("Local Test 1", "9bf8-190-215-158-88.ap.ngrok.io", reg, "80");
     console.log("Hi :)");
     console.log(":v");
-    //addServerDef("Local Test 2", "e7a3-190-215-158-88.ap.ngrok.io", reg,"80");
+    addServerDef("Local Test 2", "127.0.0.1", reg,"80");
 
     //reg = "FFA";
     //  addServerDef("FFA", "146.148.81.224", reg);
